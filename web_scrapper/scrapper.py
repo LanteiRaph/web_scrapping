7f91b85ec2b517bbe9b7bc6be314477ec8b42925
Lantei <42907968+LanteiRaph@users.noreply.github.com>
2020-11-06 08:58:07 +0300

    Add files via upload

---
+++ web_scrapper/scrapper.py
@@ -0,0 +1,19 @@
+import requests
+
+url = "http://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168"
+
+page = requests.get(url)
+
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(page.content, 'html.parser')
+
+seven_day = soup.find(id="seven-day-forecast")
+forecast_items = seven_day.find_all(class_="tombstone-container")
+tonight = forecast_items[0]
+period = tonight.find(class_="period-name").get_text()
+short_desc = tonight.find(class_="short-desc").get_text()
+temp = tonight.find(class_="temp").get_text()
+print(period)
+print(short_desc)
+print(temp)
